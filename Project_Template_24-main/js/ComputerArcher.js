4cd16b4aeaa6098ce24cf97158c72d35e80790ce
anrishe504 <84959653+anrishe504@users.noreply.github.com>
2021-08-14 09:55:23 +0530

    Add files via upload

---
+++ Project_Template_24-main/js/ComputerArcher.js
@@ -0,0 +1,23 @@
+class ComputerArcher {
+    constructor(x, y, width, height, archerAngle) {
+
+      this.w = width;
+      this.h = height;
+      this.body = Bodies.rectangle(x, y, this.w, this.h);
+      this.image = loadImage("./assets/computerArcher.png");
+      World.add(world, this.body);
+    }
+  
+   display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.w, this.h);
+      pop();
+    }
+  }
+  
